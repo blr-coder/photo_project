c4ce75097d76dc42b04ec477593e35bfa5b12113
Sania Rabushko <05011989e@gmail.com>
2020-04-12 13:54:42 +0300

    added primary settings

--- app/User.php
+++ app/User.php
@@ -10,30 +10,27 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    public const ROLES = ['superAdmin', 'Admin', 'User'];
+
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function isSuperAdmin(): bool
+    {
+        return $this->role === self::ROLES[0];
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->role === self::ROLES[1];
+    }
 }
